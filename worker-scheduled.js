0433c798f09f24f53bfc70232f95139408416d2e
Parontv <Listaconcerti@yahoo.it>
2026-09-06 16:15:24 +0200

    V1 cleanup: remove unused root legacy file

--- worker-scheduled.js
+++
@@ -1,9 +0,0 @@
-import base from './worker-v19.js';
-export default {
-  async scheduled(event, env, ctx) {
-    if (base.scheduled) return base.scheduled(event, env, ctx);
-  },
-  async fetch(req, env, ctx) {
-    return base.fetch(req, env, ctx);
-  }
-};
